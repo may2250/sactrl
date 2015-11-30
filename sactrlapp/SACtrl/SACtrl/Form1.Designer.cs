a7776d4419f4e465d63848df124ea39cab08fbd5
unknown <may2250@163.com>
2015-11-30 13:50:53 +0800

    add app demo

---
+++ sactrlapp/SACtrl/SACtrl/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace SACtrl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.btn_connect = new System.Windows.Forms.Button();
+            this.txt_ip = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.txt_center = new System.Windows.Forms.TextBox();
+            this.btn_setcenter = new System.Windows.Forms.Button();
+            this.btn_catv = new System.Windows.Forms.Button();
+            this.txt_ch = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_cnn = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_video = new System.Windows.Forms.TextBox();
+            this.txt_ctb = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_cso = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_connect
+            // 
+            this.btn_connect.Location = new System.Drawing.Point(264, 12);
+            this.btn_connect.Name = "btn_connect";
+            this.btn_connect.Size = new System.Drawing.Size(75, 29);
+            this.btn_connect.TabIndex = 0;
+            this.btn_connect.Text = "链接";
+            this.btn_connect.UseVisualStyleBackColor = true;
+            this.btn_connect.Click += new System.EventHandler(this.btn_connect_Click);
+            // 
+            // txt_ip
+            // 
+            this.txt_ip.Location = new System.Drawing.Point(66, 12);
+            this.txt_ip.Name = "txt_ip";
+            this.txt_ip.Size = new System.Drawing.Size(192, 25);
+            this.txt_ip.TabIndex = 1;
+            this.txt_ip.Text = "192.168.1.234";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "IP:";
+            // 
+            // chart1
+            // 
+            this.chart1.BackSecondaryColor = System.Drawing.Color.WhiteSmoke;
+            chartArea2.AxisX.Interval = 50D;
+            chartArea2.AxisX.IsLabelAutoFit = false;
+            chartArea2.AxisX.LabelStyle.Enabled = false;
+            chartArea2.AxisY.LabelStyle.Format = "N3";
+            chartArea2.BackColor = System.Drawing.Color.DarkGray;
+            chartArea2.Name = "ChartArea1";
+            chartArea2.Position.Auto = false;
+            chartArea2.Position.Height = 100F;
+            chartArea2.Position.Width = 100F;
+            this.chart1.ChartAreas.Add(chartArea2);
+            this.chart1.Location = new System.Drawing.Point(31, 270);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Bright;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.LabelBackColor = System.Drawing.Color.White;
+            series2.LabelBorderColor = System.Drawing.Color.White;
+            series2.MarkerColor = System.Drawing.Color.Lime;
+            series2.Name = "Series1";
+            series2.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(1066, 450);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
+            // 
+            // txt_center
+            // 
+            this.txt_center.Location = new System.Drawing.Point(395, 15);
+            this.txt_center.Name = "txt_center";
+            this.txt_center.Size = new System.Drawing.Size(100, 25);
+            this.txt_center.TabIndex = 5;
+            // 
+            // btn_setcenter
+            // 
+            this.btn_setcenter.AutoSize = true;
+            this.btn_setcenter.Location = new System.Drawing.Point(514, 15);
+            this.btn_setcenter.Name = "btn_setcenter";
+            this.btn_setcenter.Size = new System.Drawing.Size(107, 26);
+            this.btn_setcenter.TabIndex = 6;
+            this.btn_setcenter.Text = "设置中心频率";
+            this.btn_setcenter.UseVisualStyleBackColor = true;
+            this.btn_setcenter.Click += new System.EventHandler(this.btn_setcenter_Click);
+            // 
+            // btn_catv
+            // 
+            this.btn_catv.Location = new System.Drawing.Point(772, 15);
+            this.btn_catv.Name = "btn_catv";
+            this.btn_catv.Size = new System.Drawing.Size(102, 26);
+            this.btn_catv.TabIndex = 7;
+            this.btn_catv.Text = "开始测试";
+            this.btn_catv.UseVisualStyleBackColor = true;
+            this.btn_catv.Click += new System.EventHandler(this.btn_catv_Click);
+            // 
+            // txt_ch
+            // 
+            this.txt_ch.Location = new System.Drawing.Point(666, 15);
+            this.txt_ch.Name = "txt_ch";
+            this.txt_ch.Size = new System.Drawing.Size(100, 25);
+            this.txt_ch.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(900, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 15);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "载噪比CNN：";
+            // 
+            // txt_cnn
+            // 
+            this.txt_cnn.Location = new System.Drawing.Point(997, 16);
+            this.txt_cnn.Name = "txt_cnn";
+            this.txt_cnn.Size = new System.Drawing.Size(100, 25);
+            this.txt_cnn.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(900, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 15);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "载波幅度：";
+            // 
+            // txt_video
+            // 
+            this.txt_video.Location = new System.Drawing.Point(997, 55);
+            this.txt_video.Name = "txt_video";
+            this.txt_video.Size = new System.Drawing.Size(100, 25);
+            this.txt_video.TabIndex = 12;
+            // 
+            // txt_ctb
+            // 
+            this.txt_ctb.Location = new System.Drawing.Point(997, 98);
+            this.txt_ctb.Name = "txt_ctb";
+            this.txt_ctb.Size = new System.Drawing.Size(100, 25);
+            this.txt_ctb.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(900, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 15);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "CTB数值：";
+            // 
+            // txt_cso
+            // 
+            this.txt_cso.Location = new System.Drawing.Point(997, 141);
+            this.txt_cso.Name = "txt_cso";
+            this.txt_cso.Size = new System.Drawing.Size(100, 25);
+            this.txt_cso.TabIndex = 16;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(900, 144);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 15);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "CSO数值：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1123, 732);
+            this.Controls.Add(this.txt_cso);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txt_ctb);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_video);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_cnn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_ch);
+            this.Controls.Add(this.btn_catv);
+            this.Controls.Add(this.btn_setcenter);
+            this.Controls.Add(this.txt_center);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_ip);
+            this.Controls.Add(this.btn_connect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_connect;
+        private System.Windows.Forms.MaskedTextBox txt_ip;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox txt_center;
+        private System.Windows.Forms.Button btn_setcenter;
+        private System.Windows.Forms.Button btn_catv;
+        private System.Windows.Forms.TextBox txt_ch;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_cnn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_video;
+        private System.Windows.Forms.TextBox txt_ctb;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_cso;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ sactrlapp/SACtrl/SACtrl/Form1.cs
@@ -0,0 +1,401 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Text.RegularExpressions;
+
+namespace SACtrl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //CheckForIllegalCrossThreadCalls = false;
+        }
+        public const int BufferSize = 1024 * 1024 * 20;
+        public byte[] pBuffer = new byte[BufferSize];
+        float[] m_pfData;   // 数据缓冲区
+        double[] m_pData;   // 绘图缓冲区
+        int m_nLen = 0;				// 数据缓冲区长度
+        double m_dRef = 0.0;			// 参考电平
+        int m_nDBDiv = 10;			// dB/
+        Object locked = new object();  //程序锁
+        NetworkStream stream = null;
+        TcpClient tcp = null;
+        public delegate void MyInvoke();
+
+        private void ConnectCallback() 
+        {            
+            try 
+            {  
+                byte[] sendData;
+                byte[] byPackage = new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+                byte[] tmpbyte = new byte[] { 0x00, 0x00, 0x00, 0x00 };
+                int nCount = 0;
+                int res = 0, nPackage = 0;
+                int nBit = 0;
+                string strResponse = "";
+                while (true)
+                {                    
+                    nCount = 0;
+                    try
+                    {
+                        lock (locked)
+                        {
+                            // 得到参考
+                            sendData = Encoding.ASCII.GetBytes("DISPlay:WINDow:TRACe:Y:RLEVel?\r\n");
+                            stream.Write(sendData, 0, sendData.Length);     // 发往服务器
+                            do
+                            {
+                                pBuffer = new byte[BufferSize];
+                                stream.Read(pBuffer, 0, pBuffer.Length);//接受服务器回报的字符串 
+                                nCount++;
+                                if (nCount == 10)
+                                {
+                                    return;
+                                }
+                                strResponse = Encoding.ASCII.GetString(pBuffer).Trim();//从服务器接受到的字符串
+                                res = strResponse.IndexOf("\0");
+                            } while (res == 0 || strResponse.IndexOf("trac") > -1);
+                            strResponse = strResponse.Substring(strResponse.IndexOf("DISPlay:WINDow:TRACe:Y:RLEVel?\r\n"), res);
+                            if (strResponse.Length < 500)
+                            {
+                                int nStart = strResponse.IndexOf("\r\n");
+                                int nEnd = strResponse.LastIndexOf("\r\n");
+                                strResponse = strResponse.Substring(nStart + 2, nEnd - nStart - 2);
+                                m_dRef = double.Parse(strResponse);//参考值
+                            }
+                            // 得到波形
+                            sendData = Encoding.ASCII.GetBytes("trac?\r\n");
+                            stream.Write(sendData, 0, sendData.Length);
+                            nCount = stream.Read(pBuffer, 0, pBuffer.Length);//接受服务器回报的字符串
+                            strResponse = Encoding.ASCII.GetString(pBuffer).Trim();
+                            res = strResponse.IndexOf("\0");
+                            strResponse = strResponse.Substring(strResponse.IndexOf("trac?\r\n"), res);
+                            if (strResponse.Length > 0)
+                            {
+                                int nStart = strResponse.IndexOf("\r\n") + 2;
+                                nCount = nCount - nStart;
+                                Array.Copy(pBuffer, nStart, pBuffer, 0, nCount);
+                                if (pBuffer[0] == '#')//35
+                                {
+                                    nBit = pBuffer[1] - '0';//48
+                                    Array.Copy(pBuffer, 2, byPackage, 0, nBit);
+                                    nPackage = int.Parse(Encoding.ASCII.GetString(byPackage));
+                                    /* while (nCount < nPackage)
+                                    {
+                                        stream.BeginRead(pBuffer, nCount, pBuffer.Length, null, stream);
+                                        nCount += dwRecv;
+                                    }*/
+                                    Array.Copy(pBuffer, 2 + nBit, pBuffer, 0, nPackage);
+                                    nCount = nPackage / sizeof(float);
+                                    m_nLen = nCount;
+                                    m_pfData = new float[nCount];
+                                    m_pData = new double[nCount];
+                                    //Array.Copy(pBuffer, 0, m_pfData, 0, nPackage);
+                                    for (int i = 0; i < nCount; i++)
+                                    {
+                                        tmpbyte[0] = pBuffer[4 * i];
+                                        tmpbyte[1] = pBuffer[4 * i + 1];
+                                        tmpbyte[2] = pBuffer[4 * i + 2];
+                                        tmpbyte[3] = pBuffer[4 * i + 3];
+                                        m_pData[i] = m_pfData[i] = BitConverter.ToSingle(tmpbyte, 0);
+                                    }
+                                    Method_Draw();
+                                }
+                            }
+                        }                        
+                    }
+                    catch
+                    {
+                        //MessageBox.Show("error!");
+                    }
+                    finally
+                    {
+                       
+                    }
+                    Thread.Sleep(20);
+               }                    
+            
+ 
+            }catch (Exception e) {
+                MessageBox.Show("无法链接到设备!!");
+            }
+        }
+
+        private void Method_Draw()
+        {
+            //this.Invoke
+            this.BeginInvoke(new MyInvoke(chart_Draw));
+        }
+
+        private void chart_Draw()
+        {
+            if (m_nLen > 0)
+            {
+                try
+                {
+                    chart1.ChartAreas[0].AxisX.Maximum = m_nLen;
+                    //chart1.ChartAreas[0].AxisX.Interval = 50;
+                    chart1.ChartAreas[0].AxisY.Interval = 8;
+                    chart1.ChartAreas[0].AxisY.Minimum = m_dRef - m_nDBDiv * 8;
+                    chart1.ChartAreas[0].AxisY.Maximum = m_dRef;
+                    chart1.Series["Series1"].ChartType = SeriesChartType.Spline;//曲线类型 
+                    if (chart1.Series["Series1"].Points.Count > 0)
+                    {
+                        for (int i = 0; i < m_nLen; i++)
+                        {
+                            chart1.Series["Series1"].Points[i].SetValueY(m_pData[i]);
+                        }
+                    }
+                    else
+                    {
+                        for (int i = 0; i < m_nLen; i++)
+                        {
+                            chart1.Series["Series1"].Points.AddY(m_pData[i]);
+                        }
+                    }         
+                        
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message.ToString());
+                }
+            }          
+        }
+
+        private void btn_connect_Click(object sender, EventArgs e)
+        {
+            if (tcp == null)
+            {
+                tcp = new TcpClient();
+                IPAddress IP;
+                try
+                {
+                    IP = IPAddress.Parse(txt_ip.Text);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("无效的IP地址！");
+                    return;
+                }
+                Thread.CurrentThread.Name = "main";
+                try
+                {
+                    if (this.Ping(txt_ip.Text))
+                    {
+                        tcp.Connect(IP, 5025);
+                        if (tcp.Connected)
+                        {
+                            stream = tcp.GetStream();
+                            Thread worker = new Thread(ConnectCallback);
+                            worker.Name = "worker";
+                            worker.Start();
+                        }
+                        else
+                        {
+                            MessageBox.Show("设备连接失败!");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("无法连接设备，请确认设备连接正常!");
+                    }
+                }
+                catch { MessageBox.Show("设备连接失败!"); }       
+
+            }
+            
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+
+        private void btn_setcenter_Click(object sender, EventArgs e)
+        {            
+            string cmdstr = "FREQuency:CENTer " + txt_center.Text+"\r\n";           
+            lock (locked)
+            {
+                LanSendCmd(cmdstr, true);
+            }      
+        }
+
+        private bool LanSendCmd(string cmdstr, bool IsEcho)
+        {
+            byte[] send = new byte[10000];
+            byte[] recv = new byte[10000];
+            int nCount = 0;
+            string strResponse = "";
+            send = Encoding.ASCII.GetBytes(cmdstr);
+            try
+            {
+                stream.Write(send, 0, send.Length);     // 发往服务器
+                if (IsEcho)
+                {
+                    nCount = stream.Read(recv, 0, recv.Length);//接受服务器回报的字符串 
+                    if (nCount < 1)
+                    {
+                        return false;
+                    }
+                    recv[nCount] = 0;
+                    strResponse = Encoding.ASCII.GetString(recv).Trim();//从服务器接受到的字符串
+                    if (strResponse.LastIndexOf("\r\n") == 0)
+                    {
+                        return true;
+                    }
+                }
+
+            }
+            catch { }
+            return true;
+        }
+
+        private void LanSendCmd(string cmdstr, out double result)
+        {
+            byte[] send = new byte[10000];
+            byte[] recv = new byte[10000];
+            int nCount = 0;
+            string strResponse = "";
+            send = Encoding.ASCII.GetBytes(cmdstr);
+            try
+            {
+                stream.Write(send, 0, send.Length);     // 发往服务器
+                nCount = stream.Read(recv, 0, recv.Length);//接受服务器回报的字符串 
+                strResponse = Encoding.ASCII.GetString(recv).Trim();
+                if (nCount > 1)
+                {
+                    int nStart = strResponse.IndexOf("\r\n");
+                    int nEnd = strResponse.LastIndexOf("\r\n");
+                    strResponse = strResponse.Substring(nStart + 2, nEnd - nStart - 2);
+                    result = double.Parse(strResponse);//参考值
+                    return;
+                }
+            }
+            catch { }
+            result = 0;
+        }
+
+        // 通过socket读取数据，读取CString类型的数据
+        private void LanSendCmd(string cmdstr, out string szVal)
+        {
+            byte[] send = new byte[10000];
+            byte[] recv = new byte[10000];
+            int nCount = 0;
+            string strResponse = "";
+
+            send = Encoding.ASCII.GetBytes(cmdstr);
+            try
+            {
+                stream.Write(send, 0, send.Length);     // 发往服务器
+                nCount = stream.Read(recv, 0, recv.Length);//接受服务器回报的字符串 
+                strResponse = Encoding.ASCII.GetString(recv).Trim();
+                if (nCount > 1)
+                {
+                    int nStart = strResponse.IndexOf("\r\n");
+                    int nEnd = strResponse.LastIndexOf("\r\n");
+                    strResponse = strResponse.Substring(nStart + 2, nEnd - nStart - 2);
+                    szVal = strResponse;
+                    return;
+                }
+            }
+            catch { }
+            szVal = "";
+            return;
+        }
+
+        public static int GetNumber(string str)
+        {
+            int number = 0;
+            string num = null;
+            foreach (char item in str)
+            {
+                if (item >= 48 && item <= 58)
+                {
+                    num += item;
+                }
+            }
+            number = int.Parse(num);
+            return number;
+        } 
+
+        private void btn_catv_Click(object sender, EventArgs e)
+        {
+            int ch = 0;
+            double result = 0;
+            /*try
+            {
+                int.TryParse(txt_ch.Text, out ch);
+                if (ch > 10 || ch < 1)
+                {
+                    MessageBox.Show("错误的频道~~");
+                    return;
+                }
+            }
+            catch {
+                MessageBox.Show("错误的频道~~");
+                return;
+            }*/
+            ch = GetNumber(txt_ch.Text);
+            string cmdstr = "CATV:CHANnel:SELect "+ ch +"\r\n";
+            lock (locked)
+            {
+                string mode = "";
+                LanSendCmd("INSTrument?\r\n", out mode);
+                //设置频谱仪工作模式
+                LanSendCmd("INSTrument CATV\r\n", true);
+                LanSendCmd("INSTrument?\r\n", out mode);
+                //激活频道表
+                LanSendCmd("PLAN:ACTIve DS20 china\r\n", true);
+                //设置被测频道号
+                LanSendCmd(cmdstr, true);
+                //设置载噪比开关
+                LanSendCmd("ATV:CCN:STATe ON\r\n", true);
+                //设置单次扫描与否
+                LanSendCmd("ATV:CCN:CONTINUE\r\n", true);
+                //读取载噪比数值
+                LanSendCmd("ATV:CCN?\r\n", out result);
+                txt_cnn.Text = result.ToString();
+                LanSendCmd("ATV:CCN:STATe OFF\r\n", true);
+                //读取载波幅度
+                //LanSendCmd("ATV:CARR:VIDEo?\r\n", out result);
+                //txt_video.Text = result.ToString();
+                //读取CTB数值
+                //LanSendCmd("ATV:CTB?\r\n", out result);
+                //txt_ctb.Text = result.ToString();
+                //读取CSO数值
+                //LanSendCmd("ATV:CSO?\r\n", out result);
+                //txt_cso.Text = result.ToString();
+            }  
+    
+        }  
+
+        public bool Ping(string ip)
+        {
+            System.Net.NetworkInformation.Ping p = new System.Net.NetworkInformation.Ping();
+            System.Net.NetworkInformation.PingOptions options = new System.Net.NetworkInformation.PingOptions();
+            options.DontFragment = true;
+            string data = "Test Data!";
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+            int timeout = 1000; // Timeout 时间，单位：毫秒  
+            System.Net.NetworkInformation.PingReply reply = p.Send(ip, timeout, buffer, options);
+            if (reply.Status == System.Net.NetworkInformation.IPStatus.Success)
+                return true;
+            else
+                return false;
+        }
+
+        
+    }
+}
